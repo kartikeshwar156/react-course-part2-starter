14f3bdf6583be8fda0ca3edee72e16834b3f1d27
kartikeshwar <kartikeshwarhingole@gmail.com>
2024-03-10 16:46:38 +0530

    Fetching Data with React Query

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 interface Todo {
   id: number;
@@ -9,21 +9,34 @@ interface Todo {
 }
 
 const TodoList = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
-  const [error, setError] = useState('');
 
-  useEffect(() => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/todos')
-      .then((res) => setTodos(res.data))
-      .catch((error) => setError(error));
-  }, []);
+  // below we gave a new name to our 'data' which is 'todos'
+  const { data: todos } = useQuery({
+    queryKey: ['todos'],
+    queryFn: () => {
+      axios
+        .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
+        .then((res) => res.data);
 
-  if (error) return <p>{error}</p>;
+      // here get returns a promise or an object containing data, but we want to store actual data in cache so we use '.then' in this case.
+    },
+  });
+
+  // const [todos, setTodos] = useState<Todo[]>([]);
+  // const [error, setError] = useState("");
+
+  // useEffect(() => {
+  //   axios
+  //     .get("https://jsonplaceholder.typicode.com/todos")
+  //     .then((res) => setTodos(res.data))
+  //     .catch((error) => setError(error));
+  // }, []);
+
+  // if (error) return <p>{error}</p>;
 
   return (
     <ul className="list-group">
-      {todos.map((todo) => (
+      {todos?.map((todo) => (
         <li key={todo.id} className="list-group-item">
           {todo.title}
         </li>
@@ -33,3 +46,6 @@ const TodoList = () => {
 };
 
 export default TodoList;
+function useQuery(arg0: {}) {
+  throw new Error("Function not implemented.");
+}
