bcf16b7765e2734c4a2ce4c73eb030e1a3dd14e4
kartikeshwar <kartikeshwarhingole@gmail.com>
2024-03-11 14:22:32 +0530

    Parameterized Queries

--- src/react-query/Hooks/usePosts.ts
+++ src/react-query/Hooks/usePosts.ts
@@ -8,13 +8,19 @@ interface Post {
 	userId: number;
  }
 
-const usePosts = () => {
+const usePosts = (userId: number | undefined) => {
   return (
 	 useQuery<Post[], Error>({
-		queryKey: ['posts'],
+		// we are modifying the 'queryKey' as we are dealing with Hearical data and we will edit it in same way we do with actual link i.e. as we move from left to right the data becomes more specific e.g.-> (entire link)/user/uerId(like 1)/posts 
+		// query key is created by us and should be diffenent for different call, if we have a constant wuery key like ['users','posts'] then call will be made only once at the start of program.
+		queryKey: userId ? ['users', userId,'posts'] : ['posts'],
 		queryFn: () =>
 		axios
-	  .get<Post[]>("https://jsonplaceholder.typicode.com/posts")
+	  .get<Post[]>("https://jsonplaceholder.typicode.com/posts",{
+		params: {
+			userId
+		}
+	  })
 	  .then((res) => res.data),
 
 	  staleTime: 1 * 60 * 1000, // 1 min
